7cdf9e9f56670436555bcd482dbc472fc3cf5be2
cheeseliu0403 <xuancen.liu@bsu.edu>
2026-07-04 23:30:02 -0400

    Update course_count.py
    Refactor course reader to use dictionary structure

--- course_count.py
+++ course_count.py
@@ -6,33 +6,30 @@ class CourseReader:
         self.filename = filename
 
     def load_courses(self):
-        """Load course information from the course file."""
-        courses = []
-        
+        """Load course information into a dictionary."""
+        courses = {}
+
         # Read each course section from the file.
         with open(self.filename, "r") as file:
             for line in file:
                 parts = line.split()
 
-                course = {
-                    "course_number": parts[0],
+                course_number = parts[0]
+                section = {
                     "section": parts[1],
                     "days": parts[2],
                     "start_time": parts[3],
                     "end_time": parts[4]
                 }
 
-                courses.append(course)
+                if course_number not in courses:
+                    courses[course_number] = []
+
+                courses[course_number].append(section)
 
         return courses
 
     def get_available_course_numbers(self):
-        """Return a list of unique available course numbers."""
+        """Return a list of available course numbers."""
         courses = self.load_courses()
-        course_numbers = []
-
-        for course in courses:
-            if course["course_number"] not in course_numbers:
-                course_numbers.append(course["course_number"])
-
-        return course_numbers
+        return list(courses.keys())
